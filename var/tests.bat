99cad0c7b8872e5cf53e305ad6dd9ed9a40434f5
Mark Bouwman <mark@markusbouwman.nl>
2018-04-05 11:12:43 +0200

    added tests and test scripts

---
+++ var/tests.bat
@@ -0,0 +1,9 @@
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test1.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test2.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test3.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test4.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test5.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test6.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test7.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./test/test8.gm
+PAUSE
