e920279652fec9f2a996ccd25104050293ab496a
Mark Bouwman <mark@markusbouwman.nl>
2018-04-05 12:15:42 +0200

    update scripts

--- var/cache.bat
+++ var/cache.bat
@@ -1,9 +1,9 @@
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_2.infinite_run_no_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_3.infinite_run_no_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_4.infinite_run_no_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_5.infinite_run_no_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_2.invariantly_eventually_fair_shared_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_3.invariantly_eventually_fair_shared_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_4.invariantly_eventually_fair_shared_access.gm
-java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar ./cache/german_linear_5.invariantly_eventually_fair_shared_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_2.infinite_run_no_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_3.infinite_run_no_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_4.infinite_run_no_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_5.infinite_run_no_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_2.invariantly_eventually_fair_shared_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_3.invariantly_eventually_fair_shared_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_4.invariantly_eventually_fair_shared_access.gm
+java -jar .\paritygamer-1.0-SNAPSHOT-jar-with-dependencies.jar -l -t all ./cache/german_linear_5.invariantly_eventually_fair_shared_access.gm
 PAUSE
